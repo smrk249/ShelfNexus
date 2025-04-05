38832d9f01202df289f04c03bf2eb33f4ec14ea3
smrk2415 <smarakgartia2415@gmail.com>
2025-04-04 23:26:04 -0400

    add favicon and app icons, update layout metadata, and modify global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,18 @@ import "./globals.css";
 export const metadata = {
   title: ' ShelfNexus – Your Digital Shelf in the Reading Verse.',
   description: 'Explore and discover books effortlessly with ShelfNexus – a sleek, fast, and responsive platform to search titles, authors, and genres. Powered by the Books API.',
+  icons: {  
+  icon: [
+      '/favicon.ico?v=4',
+    ],
+    apple:[ 
+      '/apple-touch-icon.png?v=4',
+    ],
+    shortcut:[
+      '/apple-touch-icon.png',
+    ]
+  },
+  manifest: '/site.webmanifest'
 }
 
 export default function RootLayout({
@@ -11,6 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/icon.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
